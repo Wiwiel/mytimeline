0f8cacf05fe6c4a009b811e02751a4d94db9f63f
Wiwiel <vera,y,zhukova@gmail.com>
2023-08-03 12:52:44 +0400

    Initial commit

---
+++ src/main/java/com/wiwiel/mytimeline/MyTimelineApplication.java
@@ -0,0 +1,13 @@
+package com.wiwiel.mytimeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTimelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTimelineApplication.class, args);
+	}
+
+}
